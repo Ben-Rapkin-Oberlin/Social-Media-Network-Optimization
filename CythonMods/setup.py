938454a031e282df67463d1d3e3dec0f1f01c7c5
Ben Rapkin <brapkin@oberlin.edu>
2024-01-10 23:29:16 +0800

    commenting

--- CythonMods/setup.py
+++ CythonMods/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
+#Cython conflicts with __init__.py files
 extension = [Extension("NK_landscape", ["NK_landscape.pyx"]),
               Extension("graph", ["graph.pyx"])]
 
